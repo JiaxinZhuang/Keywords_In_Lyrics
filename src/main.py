8365fb2ad960a4e5d1f5c277e5d102cb5e75401e
lincolnz9511 <lincolnz9511@gmail.com>
2018-01-11 19:05:05 +0800

    udpate

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from Lyrics_Download import Music
 from Classifier import Classifier
 
 if __name__=='__main__':
-    top = 30
+    top = 50
     classifier = Classifier()
     music = Music(singer_id=6452)
     get_lyric = music.get_lyrics_Of_singer()
@@ -10,11 +10,12 @@ if __name__=='__main__':
         try:
             sentences = next(get_lyric)
             classifier.update(sentences)
-            print('top:{}'.format(top))
+            print('\ntop:{}'.format(top))
             print(classifier.most_common(top))
+            print('\n')
         except StopIteration:
             print('No songs any more')
             break
-    print('top:{}'.format(top))
-    print(Classifier.most_common(top))
+    #print('top:{}'.format(top))
+    #print(Classifier.most_common(top))
 
